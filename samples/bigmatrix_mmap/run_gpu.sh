c3c5f17df4c36310788dafba364102d996b22580
Jasper <jasperhumblebee@gmail.com>
2021-10-10 21:50:49 -0400

    Update bigmatrix, grep_text, img, memcopy compilation and run script, no all apps can run

---
+++ samples/bigmatrix_mmap/run_gpu.sh
@@ -0,0 +1,2 @@
+./bigmatrix_cpu try 131072 560
+./fs 10 14 256 try_vector try_matrix out
---
+++ samples/img/run_gpu.sh
@@ -0,0 +1,3 @@
+./img_cpu -g test 100 1000 900
+./fs 1 28 512 test_in outputr  test_o_0 test_o_1 test_o_2
+img_cpu -o outputr
